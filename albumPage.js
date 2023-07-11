d1c1a0fefa7f5e13ae560c140e07fc5678bad406
andreas ubertini <ubertiniandreas@gmail.com>
2023-07-11 11:17:54 +0200

    update

--- albumPage.js
+++ albumPage.js
@@ -22,12 +22,6 @@ const getAlbum = function () {
       const seconds = time - minutes * 60
       const hours = Math.floor(time / 3600)
       time = time - hours * 3600
-      function str_pad_left(string, pad, length) {
-        return (new Array(length + 1).join(pad) + string).slice(-length)
-      }
-      const finalTime =
-        str_pad_left(minutes, "0", 2) + ":" + str_pad_left(seconds, "0", 2)
-      console.log(finalTime, "time")
       document.getElementById("album-info").innerHTML = ` 
                       <img
                       src=${data.artist.picture_small}
@@ -35,10 +29,42 @@ const getAlbum = function () {
                       class="rounded-circle"
                       width="25px"
                     />  
-                    ${data.artist.name}  · ${year} · ${data.nb_tracks} brani, 
-                    
+                    ${data.artist.name}  · ${year} · ${data.nb_tracks} brani, ${minutes} min ${seconds} sec.
                     `
+      const tracks = data.tracks.data
+      tracks.forEach((element, i) => {
+        let time = element.duration
+        const date = data.release_date
+        const year = date.slice(0, 4)
+        const minutes = Math.floor(time / 60)
+        const seconds = time - minutes * 60
+        const hours = Math.floor(time / 3600)
+        time = time - hours * 3600
+        console.log(element)
+        const newRow = document.createElement("div")
+        newRow.classList.add("row")
+        newRow.innerHTML = `
+      <div
+              class="col-12 border-bottom d-flex justify-content-between justify-content-lg-evenly align-items-center px-3 gap-4 py-2"
+            >
+              <div class="d-none d-lg-flex">${i + 1}</div>
+              <div class="flex-grow-1">
+                <p class="m-0">${element.title}</p>
+                <p class="m-0">${element.artist.name}</p>
+              </div>
+              <div class="d-lg-none">
+                <i class="bi bi-three-dots-vertical"></i>
+              </div>
+              <div class="d-none d-lg-flex flex-grow-1 justify-content-between">
+                <p class="mt-0">${element.rank}</p>
+                <p class="mt-0">${minutes}:${seconds}</p>
+              </div>
+            </div>
+      `
+        document.getElementById("main").appendChild(newRow)
+      })
     })
+
     .catch((err) => {
       console.log(err)
     })
