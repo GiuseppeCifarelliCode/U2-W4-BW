405746f5f291ec6c62d3cffe164072b0c0c13ec7
GiuseppeCifarelliCode <cifarelli1995@gmail.com>
2023-07-11 12:53:17 +0200

    Added Creation of cards on album clicks

--- script/search.js
+++ script/search.js
@@ -1,14 +1,4 @@
-const input = document.getElementById("basic-addon1");
-input.addEventListener("click", function () {
-  const ricerca1 = document.querySelector(".form-control");
-  const ricerca2 = ricerca1.value;
-  console.log(ricerca2);
-  let scomparsa = document.querySelectorAll(".scompari");
-  scomparsa.forEach((e) => {
-    e.classList.add("d-none");
-  });
-
-  const URL = `https://striveschool-api.herokuapp.com/api/deezer/search?q=${ricerca2} `;
+const searchGenerator = function (URL) {
   fetch(URL)
     .then((res) => {
       if (res.ok) {
@@ -28,16 +18,45 @@ input.addEventListener("click", function () {
         let col = document.createElement("div");
         col.classList.add("col", "d-flex", "scompari");
         col.innerHTML = `<div class="card" style="width: 18rem;">
-     <img src=${element.album.cover_medium} class="card-img-top " alt="...">
-     <div class="card-body d">
-       <h5 class="card-title">${element.title} </h5>
-       <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-       <a href="#" class="btn btn-primary">Go somewhere</a>
-     </div>
-   </div>    `;
+   <img src=${element.album.cover_medium} class="card-img-top " alt="...">
+   <div class="card-body d">
+     <h5 class="card-title">${element.title} </h5>
+     <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+     <a href="#" class="btn btn-primary">Go somewhere</a>
+   </div>
+ </div>    `;
 
         riga.appendChild(col);
       });
     })
     .catch((err) => console.log(err));
+};
+
+const input = document.getElementById("basic-addon1");
+input.addEventListener("click", function () {
+  const ricerca1 = document.querySelector(".form-control");
+  const ricerca2 = ricerca1.value;
+  console.log(ricerca2);
+  let scomparsa = document.querySelectorAll(".scompari");
+  scomparsa.forEach((e) => {
+    e.classList.add("d-none");
+  });
+  const URL = `https://striveschool-api.herokuapp.com/api/deezer/search?q=${ricerca2}`;
+  console.log(URL);
+  searchGenerator(URL);
+});
+
+console.log(document.querySelectorAll(".card img"));
+
+const allImgCards = document.querySelectorAll(".card img");
+allImgCards.forEach((img) => {
+  img.addEventListener("click", function () {
+    let scomparsa = document.querySelectorAll(".scompari");
+    scomparsa.forEach((e) => {
+      e.classList.add("d-none");
+    });
+    searchGenerator(
+      `https://striveschool-api.herokuapp.com/api/deezer/search?q=${img.alt}`
+    );
+  });
 });
