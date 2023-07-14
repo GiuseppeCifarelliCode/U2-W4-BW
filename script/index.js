54f23b23f07e6f9a70a8e27e90ee71460884e6ca
GiuseppeCifarelliCode <cifarelli1995@gmail.com>
2023-07-14 10:41:40 +0200

    Added logout Button

--- script/index.js
+++ script/index.js
@@ -1,42 +1,42 @@
-let col = document.querySelectorAll(".colonna")
-console.log(col)
+let col = document.querySelectorAll(".colonna");
+console.log(col);
 col.forEach((col, i) => {
-  let query = document.querySelectorAll(".name")
-  let testo = query[i].innerText
-  console.log(testo)
+  let query = document.querySelectorAll(".name");
+  let testo = query[i].innerText;
+  console.log(testo);
 
-  let URL = `https://striveschool-api.herokuapp.com/api/deezer/search?q=${testo}`
+  let URL = `https://striveschool-api.herokuapp.com/api/deezer/search?q=${testo}`;
 
   col.addEventListener("click", () => {
     fetch(URL)
       .then((res) => {
         if (res.ok) {
-          console.log(res)
-          return res.json()
+          console.log(res);
+          return res.json();
         }
       })
 
       .then((data) => {
-        let newdata = data.data
-        console.log(newdata)
+        let newdata = data.data;
+        console.log(newdata);
         newdata.forEach((el) => {
-          console.log(`${el.album.id}`)
-          window.location.href = `albumPage.html?id=${el.album.id}`
-        })
+          console.log(`${el.album.id}`);
+          window.location.href = `albumPage.html?id=${el.album.id}`;
+        });
       })
 
       .catch((err) => {
-        console.log(err)
-      })
-  })
-})
+        console.log(err);
+      });
+  });
+});
 
-let playbutton = document.querySelector("#play")
+let playbutton = document.querySelector("#play");
 playbutton.addEventListener("click", () => {
-  let aTag = document.createElement("audio")
-  aTag.id = "play"
-  aTag.controls = true
-  aTag.autoplay = true
+  let aTag = document.createElement("audio");
+  aTag.id = "play";
+  aTag.controls = true;
+  aTag.autoplay = true;
   aTag.classList.add(
     "d-none",
     "position-fixed",
@@ -44,43 +44,34 @@ playbutton.addEventListener("click", () => {
     "start-50",
     "translate-middle-x",
     "w-50"
-  )
-  let sMP3 = document.createElement("source")
-  sMP3.classList.add("source-mp3")
-  aTag.appendChild(sMP3)
-  document.getElementById("player").appendChild(aTag)
+  );
+  let sMP3 = document.createElement("source");
+  sMP3.classList.add("source-mp3");
+  aTag.appendChild(sMP3);
+  document.getElementById("player").appendChild(aTag);
 
   sMP3.src =
-    "https://cdns-preview-3.dzcdn.net/stream/c-358eb79e55e30b4719d976e15d41e230-8.mp3"
-  console.log(sMP3)
-  sMP3.type = "audio/mp3"
-  aTag.classList.remove("d-none")
+    "https://cdns-preview-3.dzcdn.net/stream/c-358eb79e55e30b4719d976e15d41e230-8.mp3";
+  console.log(sMP3);
+  sMP3.type = "audio/mp3";
+  aTag.classList.remove("d-none");
 
-  document.querySelector("nav").appendChild(aTag)
-  playbutton.disabled = true
-})
+  document.querySelector("nav").appendChild(aTag);
+  playbutton.disabled = true;
+});
 
-<<<<<<< HEAD
 // let saveButton = document.querySelector(".save");
 // console.log();
 
 // saveButton.addEventListener("click", () => {
 //   localStorage.setItem("artista nirvana", "smell like teen spirit");
 // });
-=======
-let saveButton = document.querySelector(".save")
-console.log()
 
-saveButton.addEventListener("click", () => {
-  localStorage.setItem("artista nirvana", "smell like teen spirit")
-})
->>>>>>> main
-
-let hiddenb = document.querySelector(".hiddenb")
-console.log(hiddenb)
-let mostraB = document.createElement("button")
-mostraB.innerHTML = "MOSTRA ANNUNCI"
-document.getElementById("top").appendChild(mostraB)
+let hiddenb = document.querySelector(".hiddenb");
+console.log(hiddenb);
+let mostraB = document.createElement("button");
+mostraB.innerHTML = "MOSTRA ANNUNCI";
+document.getElementById("top").appendChild(mostraB);
 mostraB.classList.add(
   "d-none",
   "btn",
@@ -90,88 +81,84 @@ mostraB.classList.add(
   "justify-content-end",
   "border-0",
   "mostra"
-)
+);
 hiddenb.addEventListener("click", () => {
-  mostraB.classList.remove("d-none")
-  let row = document.querySelector(".rw")
-  row.classList.add("d-md-none")
-})
-let row = document.querySelector(".rw")
+  mostraB.classList.remove("d-none");
+  let row = document.querySelector(".rw");
+  row.classList.add("d-md-none");
+});
+let row = document.querySelector(".rw");
 
-console.log(mostraB)
+console.log(mostraB);
 mostraB.addEventListener("click", () => {
-  row.classList.remove("d-md-none")
-  mostraB.classList.add("d-none")
-})
+  row.classList.remove("d-md-none");
+  mostraB.classList.add("d-none");
+});
 
-<<<<<<< HEAD
-
-
-
-
-
-
-  
-=======
-let mycol = document.querySelectorAll(".cardV img ")
+let mycol = document.querySelectorAll(".cardV img ");
 // console.log(mycol);
 mycol.forEach((img, i) => {
-  let alt = img.alt
-  console.log(alt)
->>>>>>> main
+  let alt = img.alt;
+  console.log(alt);
 
   img.parentElement.addEventListener("click", () => {
     fetch(`https://striveschool-api.herokuapp.com/api/deezer/search?q=${alt}`)
       .then((res) => {
         if (res.ok) {
-          console.log(res)
-          return res.json()
+          console.log(res);
+          return res.json();
         }
       })
 
       .then((data) => {
-        window.location.href = `albumPage.html?id=${data.data[0].album.id}`
+        window.location.href = `albumPage.html?id=${data.data[0].album.id}`;
       })
       .catch((err) => {
-        console.log(err)
-      })
-  })
-})
+        console.log(err);
+      });
+  });
+});
 
 // USERNAME/ACCESS LINK
-const userButton = document.querySelector(".user-button")
-const bottomHeader = document.querySelector(".header-cardA")
-const userName = JSON.parse(localStorage.getItem("username"))
+const userButton = document.querySelector(".user-button");
+const bottomHeader = document.querySelector(".header-cardA");
+const userName = JSON.parse(localStorage.getItem("username"));
 if (userName) {
-  userButton.innerText = userName
-  bottomHeader.classList.add("d-none !important") //Non si attiva perchè ha display flex !important
+  userButton.innerText = userName;
+  bottomHeader.classList.add("d-md-none");
 } else {
-  userButton.innerText = "Sign In"
+  userButton.innerText = "Sign In";
 }
 
+// LOGOUT
+const logoutButton = document.querySelector(".logout");
+logoutButton.addEventListener("click", () => {
+  localStorage.removeItem("username");
+});
+
 const populatePlaylist = function () {
-  const playlist = JSON.parse(localStorage.getItem("playlist"))
+  const playlist = JSON.parse(localStorage.getItem("playlist"));
   if (playlist) {
     playlist.forEach((track) => {
-      const newLi = document.createElement("li")
-      newLi.innerText = track.title
+      const newLi = document.createElement("li");
+      newLi.innerText = track.title;
       newLi.addEventListener("click", function () {
-        window.location.href = `./albumPage.html?id=${track.id}`
-      })
-      document.getElementById("list").appendChild(newLi)
-    })
+        window.location.href = `./albumPage.html?id=${track.id}`;
+      });
+      document.getElementById("list").appendChild(newLi);
+    });
   } else {
-    const playlist = []
-    localStorage.setItem("playlist", JSON.stringify(playlist))
+    const playlist = [];
+    localStorage.setItem("playlist", JSON.stringify(playlist));
   }
-}
+};
 // si chiude la sezione amici al click
 document.getElementById("close").addEventListener("click", function () {
-  document.getElementById("footer").classList.add("d-lg-none")
-  document.querySelector("main").classList.add("flex-grow-1")
-})
+  document.getElementById("footer").classList.add("d-lg-none");
+  document.querySelector("main").classList.add("flex-grow-1");
+});
 // mostro la seziona amici al click
 document.getElementById("amici").addEventListener("click", function name() {
-  document.getElementById("footer").classList.remove("d-lg-none")
-})
-populatePlaylist()
+  document.getElementById("footer").classList.remove("d-lg-none");
+});
+populatePlaylist();
